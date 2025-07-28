57ac339227fe536b8e150a4fb0c24544a3b82458
Tiago Goncalves <85870899+tiagogoncalvestg@users.noreply.github.com>
2025-07-28 12:24:43 -0300

    atualizações da base de dados

--- ToDoApp.Frontend/obj/Debug/net9.0/ToDoApp.Frontend.AssemblyInfo.cs
+++ ToDoApp.Frontend/obj/Debug/net9.0/ToDoApp.Frontend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToDoApp.Frontend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+48ea49a553acb13c8a51e97a04b6d8a78d1cafce")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb16cf734a631313942a70747c85248e815f7008")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToDoApp.Frontend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToDoApp.Frontend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
